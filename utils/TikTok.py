3a092f369074ab3842ce4dfd1d1af9e23d45e200
fbabar88 <bfaizanbabar@gmail.com>
2025-04-11 00:21:02 -0400

    Update TikTok.py

--- utils/TikTok.py
+++ utils/TikTok.py
@@ -1,1 +1,8 @@
+def search_tiktok(keyword):
+    return [{
+        "title": "Mock TikTok Video",
+        "views": "500K",
+        "likes": "45K",
+        "url": "https://tiktok.com/mock"
+    }]
 
