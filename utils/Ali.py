38a37974ec17210b7a6b43d4e47b04c7b46e0b98
fbabar88 <bfaizanbabar@gmail.com>
2025-04-11 00:20:47 -0400

    Update Ali.py

--- utils/Ali.py
+++ utils/Ali.py
@@ -1,1 +1,8 @@
+def search_aliexpress(keyword):
+    return [{
+        "title": "Mock Product from AliExpress",
+        "price": "$12.99",
+        "orders": 1000,
+        "rating": 4.5
+    }]
 
